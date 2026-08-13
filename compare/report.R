725fe280931cba84ae416fbce89bef660a15dffd
Arni Magnusson <thisisarni@gmail.com>
2026-08-13 14:04:25 +1300

    Set ylim=c(0,1) and add draw line at 0.2

--- compare/report.R
+++ compare/report.R
@@ -23,16 +23,19 @@ n <- length(labels)
 
 # Plot depletion from each assessment
 taf.png("depletion")
-plot(NA, xlim=c(1952, 2024), ylim=c(0.2, 1), xlab="Year", ylab="SB/SBF=0")
-abline(h=seq(0.2, 1.0, by=0.1), col=col.grid)
-matlines(depletion[1], depletion[-1], ylim=c(0.2,1), lty=1, lwd=3, col=col.line)
-legend("bottomleft", labels, lwd=3, col=col.line, bty="n", inset=0.02,
+plot(NA, xlim=c(1952, 2024), ylim=c(0, 1), yaxs="i", xlab="Year",
+     ylab="SB/SBF=0", las=1)
+abline(h=seq(0, 1, by=0.1), col=col.grid)
+abline(h=0.2, lwd=1, lty=2)
+matlines(depletion[1], depletion[-1], lty=1, lwd=3, col=col.line)
+legend("bottomleft", labels, lwd=3, col=col.line, bty="n", inset=0.01,
        y.intersp=1.19)
 dev.off()
 
 # Excluding 2023
 taf.png("depletion_without_2026")
-plot(NA, xlim=c(1952, 2024), ylim=c(0.2, 1), xlab="Year", ylab="SB/SBF=0")
+plot(NA, xlim=c(1952, 2024), ylim=c(0, 1), yaxs="i", xlab="Year",
+     ylab="SB/SBF=0", las=1)
 abline(h=seq(0.2, 1.0, by=0.1), col=col.grid)
 matlines(dep.to.23[1], dep.to.23[-1], ylim=c(0.2,1), lty=1, lwd=3, col=col.line)
 legend("bottomleft", c(labels[-n], ""), lwd=3, col=c(col.line[-n], NA), bty="n",
